60ee5a91c502d9e0fa6d483d97c417683f691b15
Autoscatto <4utoscatto@gmail.com>
2018-10-06 15:34:03 +0200

    long/short option, versioning

--- mastojeppetto/__init__.py
+++ mastojeppetto/__init__.py
@@ -5,11 +5,10 @@ import sys
 import concurrent.futures
 from urllib.parse import urlparse
 import os
-VERSION = "0.0.4"
+VERSION = "0.0.5"
 
 logging.basicConfig()
 logging.getLogger().setLevel(logging.INFO)
-
 
 PATH = ""
 CONCURRENCY = 4
@@ -19,25 +18,22 @@ ENDPOINT = "/api/v1/custom_emojis"
 VERBOSE = False
 
 
-def th_downloader(data, path, verbose=False):
+def th_downloader(data, path):
     try:
         url = data.get('static_url')
         fname = urlparse(url).path.split('/')[-1]
         r = requests.get(url, allow_redirects=True)
         open("{}/{}".format(path, fname), 'wb').write(r.content)
-        if verbose:
-            logging.info("Downloaded {shortcode} in {fname}".format(shortcode=data.get("shortcode"), fname=fname))
+        logging.info("Downloaded {shortcode} in {fname}".format(shortcode=data.get("shortcode"), fname=fname))
         return data.get("shortcode"), fname
     except Exception as e:
-        if verbose:
-            logging.info("Downloaded {shortcode} failed: [{error}]".format(shortcode=data.get("shortcode"),
-                                                                              error=e.__repr__()))
+        logging.info("Downloaded {shortcode} failed: [{error}]".format(shortcode=data.get("shortcode"),
+                                                                       error=e.__repr__()))
 
 
 def download_all(url, path=PATH, concurrency=CONCURRENCY, text=TEXT, endpoint=ENDPOINT, verbose=VERBOSE):
     if verbose:
         logging.getLogger().setLevel(logging.DEBUG)
-
 
     try:
         r = requests.get("{base}{apiendpoint}".format(base=url, apiendpoint=endpoint))
@@ -62,7 +58,6 @@ def download_all(url, path=PATH, concurrency=CONCURRENCY, text=TEXT, endpoint=EN
             logging.info("Starting {concurrency} concurrent executor".format(concurrency=concurrency))
 
             with concurrent.futures.ThreadPoolExecutor(max_workers=concurrency) as executor:
-                # Start the load operations and mark each future with its URL
                 future_to_url = {executor.submit(th_downloader, j, download_path): j for j in data}
                 oks = []
                 for future in concurrent.futures.as_completed(future_to_url):
@@ -74,9 +69,10 @@ def download_all(url, path=PATH, concurrency=CONCURRENCY, text=TEXT, endpoint=EN
                     else:
                         oks.append(data)
 
-
             if text:
                 with open("{path}/{textfile}".format(path=download_path, textfile=TEXTFILE), "w") as textf:
+                    logging.info("Writing \"{path}/{textfile}\"".format(path=download_path, textfile=TEXTFILE))
+
                     textf.writelines([
                         "{shortcode}, /emoji/custom/{fname}\n".format(shortcode=shortcode,
                                                              fname=fname) for shortcode, fname in oks])
@@ -85,7 +81,7 @@ def download_all(url, path=PATH, concurrency=CONCURRENCY, text=TEXT, endpoint=EN
             return data
 
         except Exception as e:
-            logging.error("Connection error to mastodon base url {url}".format(url=url))
+            logging.error("Endpoint {endpoint} parsing error!".format(endpoint=endpoint))
             print(e)
             sys.exit(1)
 
@@ -96,26 +92,29 @@ def download_all(url, path=PATH, concurrency=CONCURRENCY, text=TEXT, endpoint=EN
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("url", help="Mastodon (or Pleroma) base url")
-    parser.add_argument("--path",
+    parser.add_argument("-p", "--path",
                         help="download destination [default: /tmp/${domainname}]",
                         type=str,
                         default=PATH)
-    parser.add_argument("--concurrency",
+    parser.add_argument("-c", "--concurrency",
                         help="how many parallel download processes will start [default: 4]",
                         type=int,
                         default=CONCURRENCY)
-    parser.add_argument("--text",
-                        help="generate also {} (for Pleroma) [default: True]".format(TEXTFILE),
-                        type=bool,
-                        default=TEXT)
     parser.add_argument("--endpoint",
                         help="custom api emoji endpoint (usually works without changes) "
                              "[default: \"/api/v1/custom_emojis\"]",
                         type=str,
                         default=ENDPOINT)
-    parser.add_argument("--verbose",
+    parser.add_argument("-t", "--text",
+                        help="generate also {} [default: True]".format(TEXTFILE),
+                        default=True,
+                        action="store_true")
+    parser.add_argument("-d", "--debug",
                         help="be verbose [default: False]",
                         action="store_true")
+    parser.add_argument("-v", "--version",
+                        action="version",
+                        version="%(prog)s {version}".format(version=VERSION))
     args = parser.parse_args()
 
     download_all(url=args.url,
@@ -123,4 +122,4 @@ def main():
                  concurrency=args.concurrency,
                  text=args.text,
                  endpoint=args.endpoint,
-                 verbose=args.verbose)
+                 verbose=args.debug)
