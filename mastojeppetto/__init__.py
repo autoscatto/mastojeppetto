2181691573279dff5f1b5e98338d4b478cb0c15f
Tommaso Falchi <tommaso.falchi@docomodigital.com>
2019-05-03 11:39:38 +0200

    Release finish

--- mastojeppetto/__init__.py
+++ mastojeppetto/__init__.py
@@ -5,7 +5,7 @@ import sys
 import concurrent.futures
 from urllib.parse import urlparse
 import os
-VERSION = "0.0.5"
+VERSION = "0.0.6"
 
 logging.basicConfig()
 logging.getLogger().setLevel(logging.INFO)
